449ae95f05c658f287744a346216fb0ac6248be2
OscarSebastianRuedaRojas <152028425+OscarSebastianRuedaRojas@users.noreply.github.com>
2024-08-06 08:30:15 -0500

    feat: :sparkles: Creacion de FormCreate
    Se creo el fomrulario para crear productos
    
    @trainingLeader

--- src/components/ecommerce/Ecommerce.jsx
+++ src/components/ecommerce/Ecommerce.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Header from './Header/HeaderEcommerce'
 import FooterEcommerce from './FooterEcommerce/FooterEcommerce'
+import { AddProductForm } from '../common/FormCreate/FormCreate';
 // import "./Ecommerce.css"
 export default function Ecommerce() {
+    const gamas = [
+        { id: 1, nombre: 'Gama Alta' },
+        { id: 2, nombre: 'Gama Media' },
+        { id: 3, nombre: 'Gama Baja' },
+      ];
+    
+      const proveedores = [
+        { id: 1, nombre: 'Proveedor 1' },
+        { id: 2, nombre: 'Proveedor 2' },
+        { id: 3, nombre: 'Proveedor 3' },
+      ];
     return (
         <div>
             <Header />
-            
+            <AddProductForm gamas={gamas} proveedores={proveedores} />
             <FooterEcommerce />
         </div>
         

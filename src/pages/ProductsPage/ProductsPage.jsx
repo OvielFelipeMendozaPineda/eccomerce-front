29f90f513e48bf5f17ba1887d1f962c8a31041b0
OscarSebastianRuedaRojas <152028425+OscarSebastianRuedaRojas@users.noreply.github.com>
2024-08-06 06:57:20 -0500

    feat: :sparkles: Edit Tables
    Se editaron el diseño de las tablas
    
    @trainingLeader

--- src/pages/ProductsPage/ProductsPage.jsx
+++ src/pages/ProductsPage/ProductsPage.jsx
@@ -7,14 +7,15 @@ import Button from '../../components/common/Button/Button';
 import Modal from '../../components/Modal/Modal';
 import { useState } from 'react';
 import { productInput, dropdownInput } from '../../utils/inputs/product/product';
+import { render } from '@testing-library/react';
 
 
 console.log(dropdownInput);
 
 export default function ProductsPage() {
     const products = [
-        { imageUrl: 'https://cdn.usegalileo.ai/stability/90aa788c-1e5b-4186-91ce-c2ad34320bbf.png', name: 'Coffee Mug', price: '$15', inventory: '12', status: 'Active', actions: 'Edit' },
-        { imageUrl: 'https://cdn.usegalileo.ai/stability/90aa788c-1e5b-4186-91ce-c2ad34320bbf.png', name: 'Coffee Mug', price: '$15', inventory: '12', status: 'Active', actions: 'Edit' },
+        { imageUrl: 'https://cdn.usegalileo.ai/stability/90aa788c-1e5b-4186-91ce-c2ad34320bbf.png', name: 'Coffee Mug', price: '$15', inventory: '12', status: 'Active' },
+        { imageUrl: 'https://cdn.usegalileo.ai/stability/90aa788c-1e5b-4186-91ce-c2ad34320bbf.png', name: 'Coffee Mug', price: '$15', inventory: '12', status: 'Active' },
     ];
     const headers = [
         { title: 'Product', key: 'name', className: 'table-ec883312-db4a-45d6-8895-5f43c9f4c6a2-column-176 px-4 py-3 text-left text-[#0e141b] w-[400px] text-sm font-medium leading-normal' },
@@ -28,8 +29,7 @@ export default function ProductsPage() {
                     <span className="truncate">{row.status}</span>
                 </button>
             )
-        },
-        { title: 'Actions', key: 'actions', className: 'table-ec883312-db4a-45d6-8895-5f43c9f4c6a2-column-656 px-4 py-3 text-left text-[#4f7296] w-60 text-sm font-bold leading-normal tracking-[0.015em]' }
+        }
     ];
     const [showModal, setshowModal] = useState(false)
     const handleModal = (e) => {
@@ -44,7 +44,7 @@ export default function ProductsPage() {
         }
     }
 
-    
+
     return (
         <>
             <div className='flex flex-col w-full h-screen '>
@@ -60,6 +60,6 @@ export default function ProductsPage() {
                 </div>
             </div>
             <Modal modalTitle="Crear producto" fields={productInput} dropdownFields={dropdownInput} show={showModal} handleModal={handleModal} />
-            </>
+        </>
     )
 }

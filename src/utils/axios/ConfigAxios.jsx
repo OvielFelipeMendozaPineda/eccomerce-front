48094dcf5aca253d957faac57f3b6ae617e61e03
SaintHazzard <chamitos1998@hotmail.com>
2024-08-07 07:53:20 -0500

    funciona new cliente y register bien

--- src/utils/axios/ConfigAxios.jsx
+++ src/utils/axios/ConfigAxios.jsx
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://54.86.124.70:8080',
+  baseURL: 'http://localhost:8080',
 });
 
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   console.log(token);
-  
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }

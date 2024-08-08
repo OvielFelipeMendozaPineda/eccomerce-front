2c0e15bac7c283a004942eefebd4b2072b0e8ea6
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-08-08 17:05:30 -0500

    Completar endpoints crud en Pedidos.

--- src/components/ModalEditar/ModalEditar.jsx
+++ src/components/ModalEditar/ModalEditar.jsx
@@ -24,10 +24,10 @@ export const ModalEditar = ({ objecto, show, onClose, onSave, entidad }) => {
   
     return (
       <div className="modal absolute inset-0 bg-gray-400 bg-opacity-60 flex justify-center items-center animate-fade-in">
-        <div className="bg-gray-50 p-5 rounded-lg">
-          <div className="header p-3 flex justify-between items-center">
-            <h2 className="text-2xl font-medium">Editar {entidad}</h2>
-            <Button children={<box-icon className='text-4xl' name='x-circle'></box-icon>} id='close-edit-object-btn' onClick={onClose} type='button' className='text-gray-500 hover:text-gray-700' />
+        <div className="bg-gray-50 p-10 rounded-lg">
+          <div className="header px-5 py-3 flex justify-bet gap-10  items-center">
+            <h2 className="text-xl self-start -translate-y-1 font-medium">Editar {entidad}</h2>
+            <Button children={<box-icon className='text-4xl flex justify-center items-center' name='x-circle'></box-icon>} id='close-edit-object-btn' onClick={onClose} type='button' className='text-gray-500 hover:text-gray-700' />
           </div>
           <form onSubmit={handleSubmit} className="flex flex-col gap-5">
             {Object.entries(formData)
